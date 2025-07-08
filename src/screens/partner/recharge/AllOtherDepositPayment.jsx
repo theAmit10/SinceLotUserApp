427cb8f97addbc5a028c32be0a3c5ef557058f5c
theAmit10 <prasadak66@gmail.com>
2025-07-08 18:42:39 +0530

    other payment content updated

--- src/screens/partner/recharge/AllOtherDepositPayment.jsx
+++ src/screens/partner/recharge/AllOtherDepositPayment.jsx
@@ -568,7 +568,7 @@ const AllOtherDepositPayment = () => {
                       color: COLORS.white,
                       fontFamily: FONT.Montserrat_Regular,
                     }}>
-                    Create Other
+                    New payment options
                   </Text>
                 </TouchableOpacity>
               )}
